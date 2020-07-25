d83dc2f27035fd4b3a30656563f2e8c908234d40
haru90 <nakamigawa.h@gmail.com>
2020-07-25 22:17:43 +0900

    create weather_forecast_LINE_bot

---
+++ weather_forecast_LINE_bot.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+def lambda_handler(event:, context:)
+    
+    require 'net/http'
+    require 'json'
+    require 'uri'
+    
+    res = Net::HTTP.get(URI.parse('http://api.openweathermap.org/data/2.5/weather?q=tokyo,jp&units=metric&lang=ja&appid=5f52d10cd78ff978e6a96927e68c2295'))
+    name = JSON.parse(res)['name']
+    weather = JSON.parse(res)['weather'][0]["description"]
+    message = "#{name}の天気は#{weather}です"
+    
+    res = Net::HTTP.post(URI.parse('https://notify-api.line.me/api/notify'), "message=#{message}", {'Authorization': 'Bearer PvYwuA0nQxCzITu4AvblrZ5iU2T3nZPfu82JOyTjuym'})
+    
+    # { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+    
+end
