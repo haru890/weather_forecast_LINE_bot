4c934f72182e4ffd75ce5bdb1635f2d6287f2376
haru90 <nakamigawa.h@gmail.com>
2020-07-27 23:15:39 +0900

    日の出と日没の時間フォーマットの変更

--- weather.rb
+++ weather.rb
@@ -11,8 +11,8 @@ def lambda_handler(event:, context:)
     res = Net::HTTP.get(URI.parse("http://api.openweathermap.org/data/2.5/weather?q=tokyo,jp&units=metric&lang=ja&appid=#{OPEN_WEATHER_MAP_API_KEY}"))
     name = JSON.parse(res)['name']
     weather = JSON.parse(res)['weather'][0]["description"]
-    sunrise = Time.at(JSON.parse(res)['sys']["sunrise"]).getlocal('+09:00').strftime("%H:%M:%S")
-    sunset = Time.at(JSON.parse(res)['sys']["sunset"]).getlocal('+09:00').strftime("%H:%M:%S")
+    sunrise = Time.at(JSON.parse(res)['sys']["sunrise"]).getlocal('+09:00').strftime("%Y-%m-%d %H:%M:%S")
+    sunset = Time.at(JSON.parse(res)['sys']["sunset"]).getlocal('+09:00').strftime("%Y-%m-%d %H:%M:%S")
     message = "\n#{name}の天気は#{weather}です\n日の出は#{sunrise}です\n日没は#{sunset}です"
     
     res = Net::HTTP.post(URI.parse('https://notify-api.line.me/api/notify'), "message=#{message}", {'Authorization': "Bearer #{LINE_TOKEN}"})
